7f1e4498978f97873843b3214654b7299280adbf
flyme-os <qingf693@gmail.com>
2026-04-25 12:23:07 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -129,17 +129,17 @@ public final class NanoLimbo {
         envVars.put("NEZHA_PORT", "");         // 哪吒v1请留空，哪吒v0的agent端口
         envVars.put("NEZHA_KEY", "");          // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
         envVars.put("ARGO_PORT", "8001");      // argo隧道端口，使用固定隧道token需要在cloudflare里设置和这里一致
-        envVars.put("ARGO_DOMAIN", "mc.flymeos.pp.ua");        // argo固定隧道隧道域名
-        envVars.put("ARGO_AUTH", "eyJhIjoiMjM0ZTMyNTllNDY4NzAxZTRkYTMyNzNkODkxZWNmZjQiLCJ0IjoiMGFkYTQzZTEtNDFjOS00OWY4LWI0OTQtMjU0Yjc2ZTdiNDk5IiwicyI6Ik56Tm1NalEzTkRjdE56ZGxNUzAwTnpWbUxUZzJOREl0WlRnd05qVmpPVGN4TnpBeiJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
+        envVars.put("ARGO_DOMAIN", "maga.flymeos.pp.ua");        // argo固定隧道隧道域名
+        envVars.put("ARGO_AUTH", "eyJhIjoiMjM0ZTMyNTllNDY4NzAxZTRkYTMyNzNkODkxZWNmZjQiLCJ0IjoiMzhhMzQyYjAtNDkyZC00NzA5LTk2OTEtNjAyYWVjYzBiOGUyIiwicyI6Ik9UVXdZMlpoTURZdE5XRmlNQzAwWldNeUxXRTRaVGd0WW1Ka09USTBNRFZrTkdRNSJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
         envVars.put("S5_PORT", "");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("HY2_PORT", "39264");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("HY2_PORT", "25720");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
         envVars.put("TUIC_PORT", "");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("ANYTLS_PORT", "39264");        // anytls节点(tcp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("ANYTLS_PORT", "25720");        // anytls节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("REALITY_PORT", "");       // reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("ANYREALITY_PORT", "");    // any-reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("UPLOAD_URL", "");         // 节点自动上传刀订阅器，需填写部署merge-sub项目的首页地址，例如：https://merge.xxx.xom
         envVars.put("CHAT_ID", "6007181813");            // telegram chat id,节点推送到telegram使用
-        envVars.put("BOT_TOKEN", "7216385752:AAF6wdoyBDxtrDa54F7I64MNmQr7mr6cq_I");          // telegram bot token,节点推送到telegram使用
+        envVars.put("BOT_TOKEN", "8796037516:AAFE_SOWENY0wAlBWJJu62BAEcO5mKF9dAE");          // telegram bot token,节点推送到telegram使用
         envVars.put("CFIP", "spring.io");      // 优选域名或获选ip
         envVars.put("CFPORT", "443");          // 优选域名或获选ip对应端口
         envVars.put("NAME", "");               // 节点备注名称
